7bc4d545d0027917c38a513f723fd63c41e58a57
Marlina Lubis <lubismarlina5@gmail.com>
2023-03-30 12:43:33 +0700

    Marlina

--- controller/coba.go
+++ controller/coba.go
@@ -1,17 +1,157 @@
 package controller
 
 import (
+	"net/http"
+
+	inimodel "github.com/MarlinaLubis/be_bap/model"
+	inimodul "github.com/MarlinaLubis/be_bap/module"
+	"github.com/MarlinaLubis/marlinapp/config"
 	"github.com/aiteung/musik"
 	cek "github.com/aiteung/presensi"
 	"github.com/gofiber/fiber/v2"
-	"github.com/MarlinaLubis/marlinapp/config"
 )
+
+func Home(c *fiber.Ctx) error {
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"github_repo": "https://github.com/MarlinaLubis/marlinapp",
+		"message":     "You are at the root endpoint 😉",
+		"success":     true,
+	})
+}
 
 func Homepage(c *fiber.Ctx) error {
 	ipaddr := musik.GetIPaddress()
 	return c.JSON(ipaddr)
 }
+
+func GetPresensiold(c *fiber.Ctx) error {
+	nl := cek.GetPresensiCurrentMonth(config.Ulbimongoconn)
+	return c.JSON(nl)
+}
+
 func GetPresensi(c *fiber.Ctx) error {
-	ps := cek.GetPresensiCurrentMonth(config.Ulbimongoconn)
-	return c.JSON(ps)
+	nl := inimodul.GetPresensiFromCheckin("Masuk", config.Ulbimongoconn, "presensi")
+	return c.JSON(nl)
+}
+
+func GetMahasiswa(c *fiber.Ctx) error {
+	nl := inimodul.GetMahasiswaFromNama("Angelina", config.Ulbimongoconn, "mahasiswa")
+	return c.JSON(nl)
+}
+
+func GetDosen(c *fiber.Ctx) error {
+	nl := inimodul.GetDosenFromNama("Indra Riksa", config.Ulbimongoconn, "dosen")
+	return c.JSON(nl)
+}
+
+func GetJamSidang(c *fiber.Ctx) error {
+	nl := inimodul.GetJamSidangFromDurasi(7, config.Ulbimongoconn, "jamsidang")
+	return c.JSON(nl)
+}
+
+func GetBap(c *fiber.Ctx) error {
+	nl := inimodul.GetBapFromJudul("Aplikasi Perpustakaan Berbasis Web", config.Ulbimongoconn, "bap")
+	return c.JSON(nl)
+}
+
+func InsertPresensi(c *fiber.Ctx) error {
+	db := config.Ulbimongoconn
+	var presensi inimodel.Presensi
+	if err := c.BodyParser(&presensi); err != nil {
+		return err
+	}
+	insertedID := inimodul.InsertPresensi(db, "presensi",
+		presensi.Longitude,
+		presensi.Latitude,
+		presensi.Location,
+		presensi.Phone_number,
+		presensi.Checkin,
+		presensi.Biodata)
+	return c.JSON(map[string]interface{}{
+		"status":      http.StatusOK,
+		"message":     "Data Mahasiswa Berhasil Disimpan.",
+		"inserted_id": insertedID,
+	})
+}
+
+func InsertMahasiswa(c *fiber.Ctx) error {
+	db := config.Ulbimongoconn
+	var mahasiswa inimodel.Mahasiswa
+	if err := c.BodyParser(&mahasiswa); err != nil {
+		return err
+	}
+	insertedID := inimodul.InsertMahasiswa(db, "mahasiswa",
+		mahasiswa.Nama,
+		mahasiswa.Phone_number,
+		mahasiswa.Email,
+		mahasiswa.Jam_sidang,
+		mahasiswa.Harisidang)
+	return c.JSON(map[string]interface{}{
+		"status":      http.StatusOK,
+		"message":     "Data Mahasiswa Berhasil Disimpan.",
+		"inserted_id": insertedID,
+	})
+}
+
+func InsertDosen(c *fiber.Ctx) error {
+	db := config.Ulbimongoconn
+	var dosen inimodel.Dosen
+	if err := c.BodyParser(&dosen); err != nil {
+		return err
+	}
+	insertedID := inimodul.InsertDosen(db, "dosen",
+
+		dosen.Nama,
+		dosen.Phone_number,
+		dosen.Email,
+		dosen.Jam_sidang,
+		dosen.Hari_sidang)
+	return c.JSON(map[string]interface{}{
+		"status":      http.StatusOK,
+		"message":     "Data Dosen Berhasil Disimpan.",
+		"inserted_id": insertedID,
+	})
+}
+
+func InsertJamSidang(c *fiber.Ctx) error {
+	db := config.Ulbimongoconn
+	var jamsidang inimodel.JamSidang
+	if err := c.BodyParser(&jamsidang); err != nil {
+		return err
+	}
+	insertedID := inimodul.InsertJamSidang(db, "jamsidang",
+		jamsidang.Durasi,
+		jamsidang.Jam_masuk,
+		jamsidang.Jam_keluar,
+		jamsidang.Gmt,
+		jamsidang.Hari)
+	return c.JSON(map[string]interface{}{
+		"status":      http.StatusOK,
+		"message":     "Data Jam Sidang Berhasil Disimpan.",
+		"inserted_id": insertedID,
+	})
+}
+
+func InsertBap(c *fiber.Ctx) error {
+	db := config.Ulbimongoconn
+	var bap inimodel.Bap
+	if err := c.BodyParser(&bap); err != nil {
+		return err
+	}
+	insertedID := inimodul.InsertBap(db, "bap",
+		bap.Judul,
+		bap.Nama_Mahasiswa,
+		bap.Prodi,
+		bap.Hasil_revisi,
+		bap.Dosen)
+	return c.JSON(map[string]interface{}{
+		"status":      http.StatusOK,
+		"message":     "Data Bap Berhasil Disimpan.",
+		"inserted_id": insertedID,
+	})
+}
+
+func GetAll(c *fiber.Ctx) error {
+	nl := inimodul.GetAllBapFromJudul("Aplikasi Perpustakaan Berbasis Web", config.Ulbimongoconn, "bap")
+	return c.JSON(nl)
 }
--- url/url.go
+++ url/url.go
@@ -10,6 +10,16 @@ import (
 func Web(page *fiber.App) {
 	page.Post("/api/whatsauth/request", controller.PostWhatsAuthRequest)  //API from user whatsapp message from iteung gowa
 	page.Get("/ws/whatsauth/qr", websocket.New(controller.WsWhatsAuthQR)) //websocket whatsauth
-	page.Get("/", controller.Homepage) //ujicoba panggil package musik
-    page.Get("/presensi", controller.GetPresensi)
+	page.Get("/", controller.Home)                                        //ujicoba panggil package musik
+	page.Get("/presensi", controller.GetPresensi)
+	page.Get("/mahasiswa", controller.GetMahasiswa)
+	page.Get("/dosen", controller.GetDosen)
+	page.Get("/jamsidang", controller.GetJamSidang)
+	page.Get("/bap", controller.GetBap)
+	page.Get("/all", controller.GetAll)
+	page.Get("/inspresensi", controller.InsertPresensi)
+	page.Get("/insmahasiswa", controller.InsertMahasiswa)
+	page.Get("/insdosen", controller.InsertDosen)
+	page.Get("/insjamsidang", controller.InsertJamSidang)
+	page.Get("/insbap", controller.InsertBap)
 }
