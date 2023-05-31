9175ceb28b1fbd66e6b98cc578717738521bc23c
Marlina Lubis <lubismarlina5@gmail.com>
2023-05-31 14:37:11 +0700

    Latihan_Week11

--- main.go
+++ main.go
@@ -13,10 +13,21 @@ import (
 	"github.com/MarlinaLubis/marlinapp/url"
 
 	"github.com/gofiber/fiber/v2"
-	
+
 	_ "github.com/MarlinaLubis/marlinapp/docs"
 )
 
+//@title TES SWAG
+//@version 1.0
+//@description This is a sample server.
+
+//@contact.name API Support
+//@contact.url https://github.com/MarlinaLubis
+//@contact.email 1214040@std.ulbi.ac.id
+
+//@host lubisapp.herokuapp.com
+//@BasePath / 
+//@schemes https http
 func main() {
 	go whatsauth.RunHub()
 	site := fiber.New(config.Iteung)
