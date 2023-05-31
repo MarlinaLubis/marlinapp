d848794440a1344a4be84093c365e97b6184af56
Marlina Lubis <lubismarlina5@gmail.com>
2023-05-31 15:24:27 +0700

    Latihan_Week11

--- docs/docs.go
+++ docs/docs.go
@@ -175,7 +175,7 @@ const docTemplate = `{
             "properties": {
                 "_id": {
                     "type": "string",
-                    "example": "123456789"
+                    "example": "12345678901234567892"
                 },
                 "hari_kerja": {
                     "type": "array",
@@ -217,7 +217,7 @@ const docTemplate = `{
             "properties": {
                 "_id": {
                     "type": "string",
-                    "example": "123456789"
+                    "example": "12345678901234567892"
                 },
                 "biodata": {
                     "$ref": "#/definitions/controller.Karyawan"
